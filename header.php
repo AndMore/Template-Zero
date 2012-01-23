52b71730cdd410c334b361ac6cbf3cb89ee090e7
AndMore <contact.and.more@gmail.com>
2012-01-23 11:37:06 +0100

    Terza Puntata

---
+++ header.php
@@ -0,0 +1,40 @@
+<!-- Inizio Header -->
+ 
+<!doctype html>
+ 
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html lang="en"> <![endif]-->
+<!--[if IE 7]>    <html lang="en"> <![endif]-->
+<!--[if IE 8]>    <html lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+ 
+<head>
+ 
+<meta charset="utf-8">
+ 
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+ 
+<title></title>
+ 
+<meta name="description" content="">
+ 
+<meta name="author" content="">
+ 
+<meta name="viewport" content="width=device-width,initial-scale=1">
+ 
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url'); ?>">
+ 
+<link rel="stylesheet/less" type="text/css" href="<?php bloginfo( 'template_url'); ?>/less/style.less">
+<script src="<?php bloginfo( 'template_url'); ?>/js/libs/less.js" type="text/javascript"></script>
+ 
+</head>
+ 
+<body>
+ 
+<div id="container">
+ 
+<header>
+ 
+</header>
+ 
+<!-- Fine Header -->
